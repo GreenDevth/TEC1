a60f4e4ffae2542183c355834a9c94981e12aece
GreenDevth <theerapong@rumail.ru.ac.th>
2022-02-25 15:54:56 +0700

    Add checkout function

--- database/Store.py
+++ database/Store.py
@@ -3,6 +3,7 @@ from database.db_config import read_db_config
 
 db = read_db_config()
 
+
 def check_queue():
     try:
         conn = MySQLConnection(**db)
@@ -27,3 +28,46 @@ def get_pack(order):
             return res[0]
     except Error as e:
         print(e)
+
+
+def get_queue(product_code):
+    try:
+        conn = MySQLConnection(**db)
+        cur = conn.cursor()
+        cur.execute('SELECT steam_id, package_name FROM scum_shopping_cart WHERE order_number = %s', (product_code,))
+        row = cur.fetchone()
+        while row is not None:
+            data = list(row)
+            return data
+    except Error as e:
+        print(e)
+
+
+def get_package(pack_name):
+    try:
+        dbconfig = read_db_config()
+        conn = MySQLConnection(**dbconfig)
+        cur = conn.cursor()
+        cur.execute('SELECT package_data FROM scum_package WHERE package_name = %s', (pack_name,))
+        row = cur.fetchone()
+        while row is not None:
+            data = list(row)
+            return data[0]
+    except Error as e:
+        print(e)
+
+
+def delete_row():
+    conn = None
+    try:
+        dbconfi = read_db_config()
+        conn = MySQLConnection(**dbconfi)
+        cur = conn.cursor()
+        cur.execute('DELETE FROM scum_shopping_cart LIMIT 1')
+        conn.commit()
+        cur.close()
+    except Error as e:
+        print(e)
+    finally:
+        if conn is not None and conn.is_connected():
+            conn.close()
--- extension/Command_Events.py
+++ extension/Command_Events.py
@@ -1,16 +1,18 @@
 import asyncio
-from discord.ext import commands
-from discord_components import Button, ButtonStyle
-from controller.scum_ai import cmd
-import pyperclip
+import time
 from datetime import datetime
+
+from discord.ext import commands
+
+from controller.scum_ai import cmd
+from database.Store import check_queue, get_queue, get_package, delete_row
 now = datetime.now()
-time = now.strftime("%H:%M:%S")
+times = now.strftime("%H:%M:%S")
+
 
 class CommandEvents(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -18,14 +20,42 @@ class CommandEvents(commands.Cog):
         cmd_channel = self.bot.get_channel(927796274676260944)
         if message.content.startswith('.set'):
             if message.author.guild_permissions.administrator:
-                pyperclip.copy(message.content[5:])
-                txt_command = pyperclip.paste()
-                cmd(txt_command)
-                await cmd_channel.send(f'```ini\nTime : [{time}] Name : [{member.name}] Command : [{txt_command}]\n```')
+                msg = message.content[5:]
+                cmd(msg)
+                await cmd_channel.send(f'```ini\nTime : [{times}] Name : [{member.name}] Command : [{msg}]\n```')
             else:
                 await message.channel.send('คุณไม่ได้รับสิทธิ์ในการใช้งานคำสั่งเหล่านี้', delete_after=3)
                 await asyncio.sleep(3.5)
                 await message.delete()
+        if message.content.startswith('!checkout'):
+            if message.author.guild_permissions.administrator:
+                msg = message.content[10:]
+                data = get_queue(msg)
+                steam_id = data[0]
+                package = get_package(data[1])
+                spawn_code = package.split(",")
+                while True:
+                    count = check_queue()
+                    if count != 0:
+                        time.sleep(1)
+                        for x in spawn_code:
+                            time.sleep(0.5)
+                            cmd("{} location {}".format(x, steam_id))
+                            await cmd_channel.send(
+                                f'```ini\nTime : [{times}] Command : [{x} Location {steam_id}]\n```'
+                            )
+                        delete_row()
+                        time.sleep(5)
+                        await message.channel.send('คิวในการส่งของตอนนี้ เหลือ {} คิว'.format(count))
+
+                    else:
+                        await message.channel.send('คิวในการส่งของตอนนี้ เหลือ {} คิว'.format(count))
+                        return
+
+
+
+
+
 
 def setup(bot):
     bot.add_cog(CommandEvents(bot))
